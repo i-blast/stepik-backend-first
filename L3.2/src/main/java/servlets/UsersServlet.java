dd5305807413f2eaa3a940375a1e7ad9341d2205
ilYa <ilya.poloskov@gmail.com>
2022-12-16 06:36:38 +0300

    add L3.2 homework template

---
+++ L3.2/src/main/java/servlets/UsersServlet.java
@@ -0,0 +1,123 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author ilYa
+ */
+public class UsersServlet extends HttpServlet {
+
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
+    private final AccountService accountService;
+
+    public UsersServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get public user profile
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (Objects.isNull(profile)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else {
+            profileResponse(response, profile);
+        }
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (Objects.nonNull(profile)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+        } else {
+            profile = new UserProfile(login, pass, email);
+            accountService.addOrUpdateUser(profile);
+            profileResponse(response, profile);
+        }
+    }
+
+    //change profile
+    public void doPut(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (Objects.isNull(profile)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else {
+            profile = new UserProfile(login, pass, email);
+            accountService.addOrUpdateUser(profile);
+            profileResponse(response, profile);
+        }
+    }
+
+    //unregister
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (Objects.isNull(profile)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    private static void profileResponse(HttpServletResponse response, UserProfile profile) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(profile);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println(json);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
