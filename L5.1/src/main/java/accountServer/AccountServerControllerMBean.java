4fab4112d18830003df5284bfb0fc62a38082d75
ilYa <ilya.poloskov@gmail.com>
2022-12-19 12:02:17 +0300

    add homework #5 template

---
+++ L5.1/src/main/java/accountServer/AccountServerControllerMBean.java
@@ -0,0 +1,18 @@
+package accountServer;
+
+/**
+ * @author a.akbashev
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+@SuppressWarnings("UnusedDeclaration")
+public interface AccountServerControllerMBean {
+    int getUsers();
+
+    int getUsersLimit();
+
+    void setUsersLimit(int usersLimit);
+}
